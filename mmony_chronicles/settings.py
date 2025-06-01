64a831d84e54566c3b355fa189155cd6134511f8
iPelino <pelmut2000@gmail.com>
2025-06-01 13:03:18 +0200

    changed db to postgresql

--- mmony_chronicles/settings.py
+++ mmony_chronicles/settings.py
@@ -69,11 +69,24 @@ WSGI_APPLICATION = 'mmony_chronicles.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+#
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'momony-database',
+        'USER': 'wujcqsztgv',
+        'PASSWORD': 'LZBOWPe0pgM6A2$8',
+        'HOST': 'momony-server.postgres.database.azure.com',
+        'PORT': 5432,
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+        'CONN_MAX_AGE': 60,
+  # Use SSL for secure connection
+
+        # Set to your database host
+          # Default PostgreSQL port
+
     }
 }
 
